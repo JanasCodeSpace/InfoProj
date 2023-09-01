32958f3dd989e9dba5894b59159cf844414c4729
lireal <lireal@gmx.de>
2023-09-01 11:36:26 +0200

    Doxygen aktualisiert

--- RobPathGen/doc/html/search/searchdata.js
+++ RobPathGen/doc/html/search/searchdata.js
@@ -1,36 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefgilmopqrstuwxyz~",
-  1: "cgqu",
-  2: "qu",
-  3: "egilmpqrsu",
-  4: "acdglmopqrs~",
-  5: "abcdefilmopstuwxyz",
-  6: "_mq",
-  7: "b"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "defines",
-  7: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "Alle",
-  1: "Klassen",
-  2: "Namensbereiche",
-  3: "Dateien",
-  4: "Funktionen",
-  5: "Variablen",
-  6: "Makrodefinitionen",
-  7: "Seiten"
 };
 
